3b93201f9bb96ff2d80a3898604ae201f08c521f
MystoganF <keanmaverickaaligue@gmail.com>
2026-03-05 22:41:43 +0800

    backend(SpringBoot): initialized CebuNest backend file

---
+++ backend/src/main/java/com/cebunest/backend/CebuNestApplication.java
@@ -0,0 +1,13 @@
+package com.cebunest.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CebuNestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CebuNestApplication.class, args);
+	}
+
+}
